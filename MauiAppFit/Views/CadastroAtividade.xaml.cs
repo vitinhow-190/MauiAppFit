3513403f82d85bf3424834e2349e448c077fc260
231471 <231471@CPD02-M07>
2025-08-13 14:55:46 -0300

    13/08

--- MauiAppFit/Views/CadastroAtividade.xaml.cs
+++ MauiAppFit/Views/CadastroAtividade.xaml.cs
@@ -1,9 +1,78 @@
-namespace MauiAppFit.Views;
+using System.ComponentModel;
 
-public partial class CadastroAtividade : ContentPage
+
+
+namespace MauiAppFit.ViewModels
 {
-	public CadastroAtividade()
-	{
-		InitializeComponent();
-	}
+    [QueryProperty("PegarIdDaNavegacao", "parametro_id")]
+    public class CadastroAtividadeViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        string descricao, observacoes;
+        int id;
+        DateTime data;
+        double? peso;
+
+
+        public string PegarIdDaNavegacao
+        {
+            set
+            {
+                int id_parametro = Convert.ToInt32(Uri.UnescapeDataString(value));
+                VerAtividade.Execute(id_parametro);
+            }
+        }
+
+        public string Descricao
+        {
+            get => descricao;
+            set
+            {
+                descricao = value;
+                PropertyChanged(this, new PropertyChangedEventArgs("descricao"));
+            }
+        }
+
+        public string Observacoes
+        {
+            get => observacoes;
+            set
+            {
+                observacoes = value;
+                PropertyChanged(this, new PropertyChangedEventArgs("observacoes"));
+            }
+        }
+
+        public int Id
+        {
+            get => id;
+            set
+            {              
+                id = value;
+                PropertyChanged(this, new PropertyChangedEventArgs("Id"));
+               
+            }
+        }
+
+        public DateTime Data
+        {
+            get => data;
+            set
+            {
+                data = value;
+                PropertyChanged(this, new PropertyChangedEventArgs("Data"));
+            }
+        }
+
+        public double? Peso
+        {
+            get => peso;
+            set
+            {
+                peso = value;
+                PropertyChanged(this, new PropertyChangedEventArgs("Peso"));
+            }
+        }
+    }
 }
